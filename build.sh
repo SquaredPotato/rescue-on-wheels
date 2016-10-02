1c2c88807e07176469f33ae3351bba2f8c135160
Raspberry PI <pi@pi.com>
2016-10-02 20:24:06 +0000

    Cleanup: removed obsolete build.sh and added comments to run.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#cmake CMakeLists.txt && make
-
-gcc -o main main.c -lwiringPi -lm
--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+# Create make files
+cmake CMakeLists.txt
+
+# Restart on disconnect
 while true; do
+    # Build the project
     make
+
+    # Run the project
     sudo ./column
+
+    # Wait two seconds before restarting
     sleep 2
 done
